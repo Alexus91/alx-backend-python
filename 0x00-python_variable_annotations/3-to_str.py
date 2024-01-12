25c63cc58256a5a0fe0a0c7cba8d8230c22cbe6b
Alexus91 <mrfadlkamal@gmail.com>
2024-01-12 00:04:09 +0000

    4. Define variables && fix requ pycodestyle

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-""" Description: function to_str that takes a float n as argument and returns the
-string representation of the float.
+""" Description: function to_str that takes a float n as argument
+and returns the string representation of the float.
     Arguments: n: float
 """
 
