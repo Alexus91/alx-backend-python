c1375c8e2e6d3874dc7b980bc6c504c6a4f7e24d
root <Alexus91>
2024-01-15 09:47:54 +0000

    try another approch to fix the output

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,12 +6,15 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """
-    Asynchronous routine that spawns wait_random.
+    """ Let's execute multiple coroutines at the same time with async  """
+    delay_coroutines: List[float] = []
+    all_delays: List[float] = []
 
-    :param n: Number of times to spawn wait_random.
-    :param max_delay: The maximum delay for each wait_random call.
-    :return: List of delays in ascending order.
-    """
-    delays = [wait_random(max_delay) for _ in range(n)]
-    return await asyncio.gather(*delays)
+    for _ in range(n):
+        delay_coroutines.append(wait_random(max_delay))
+
+    for delay_task in asyncio.as_completed(delay_coroutines):
+        earliest_result = await delay_task
+        all_delays.append(earliest_result)
+
+    return all_delays
