1405de8e81dc7b5a7ba1daddfd84e37a7dda4d2d
root <Alexus91>
2024-02-05 16:03:52 +0000

    try a new aprroch in  3. Parameterize and patch especially in class TestMemoize(unittest.TestCase):

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -52,25 +52,22 @@ class TestGetJson(unittest.TestCase):
 
 
 class TestMemoize(unittest.TestCase):
+    """Tests the `memoize` function."""
+    def test_memoize(self) -> None:
+        """Tests `memoize`'s output."""
+        class TestClass:
+            def a_method(self):
+                return 42
 
-    class TestClass:
-        def a_method(self):
-            return 42
-
-        @memoize
-        def a_property(self):
-            return self.a_method()
-
-    @patch.object(TestClass, 'a_method')
-    def test_memoize(self, mock_a_method):
-        """
-        Test that when calling a_property twice, the correct result is returned
-        but a_method is only called once using assert_called_once.
-        """
-        mock_a_method.return_value = 42
-        test_instance = self.TestClass()
-        result_1 = test_instance.a_property()
-        result_2 = test_instance.a_property()
-        mock_a_method.assert_called_once()
-        self.assertEqual(result_1, 42)
-        self.assertEqual(result_2, 42)
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(
+                TestClass,
+                "a_method",
+                return_value=lambda: 42,
+                ) as memo_fxn:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            memo_fxn.assert_called_once()
